67b83cf2766a1b92a9b931850b07eed533cee9e7
FanJae <hjjang45@gmail.com>
2024-10-28 14:45:50 +0900

    [Silver II] Title: 에디터, Time: 164 ms, Memory: 20880 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <list>
+using namespace std;
+list<char> editor;
+list<char>::iterator cursor;
+int main(void)
+{
+	string data;
+	int command;
+	cin >> data;
+	for (int i = 0; i < data.length(); i++)
+	{
+		editor.push_back(data[i]);
+	}
+	cin >> command;
+	cursor = editor.end();
+	for (int i = 0; i < command; i++)
+	{
+		char op;
+		char insert_value;
+		cin >> op;
+		if (op == 'L')
+		{
+			if (cursor != editor.begin())
+			{
+				cursor--;
+			}
+		}
+		else if (op == 'D')
+		{
+			if (cursor != editor.end())
+			{
+				cursor++;
+			}
+		}
+		else if (op == 'B')
+		{
+			
+			if (cursor != editor.begin())
+			{
+				cursor--;
+				cursor = editor.erase(cursor);
+			}
+		}
+		else if (op == 'P')
+		{
+			cin >> insert_value;
+			editor.insert(cursor, insert_value);
+		}
+	}
+	for (auto iter = editor.begin(); iter != editor.end(); iter++)
+	{
+		cout << *iter;
+	}
+}
