130039d3413d30a3d648c1be6e102a5447e4ed16
FanJae <hjjang45@gmail.com>
2025-06-13 20:47:30 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -1,32 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
-int n,s;
-int data[105]={0};
-int count = 0;
-void back(int k, int sum)
+#include <iostream>
+using namespace std;
+int ans = 0; // 부분 수열 개수
+int n, s;
+int value[25];
+void sub(int sum, int start,int count, bool last)
 {
-	if(k == n)
+	if (count > 0 && sum == s && last == true) // 1개 이상 골랐는가 + 마지막 값 포함 여부를 판단. (중복처리)
 	{
-		if(sum == s)
-		{
-			count++;
-		}
+		ans++;
+	}
+
+	if (start >= n)
+	{
 		return ;
 	}
-	back(k+1,sum+data[k]); // 현재 데이터 포함 
-	back(k+1,sum); // 자신의 위치 데이터 미포함 
+
+	sub(sum + value[start], start + 1, count + 1, true); // 포함
+	sub(sum, start + 1, count, false); // 미포함
+
 }
 int main(void)
 {
-	scanf("%d %d",&n,&s);
-	for(int i=0; i<n; i++)
+	cin >> n >> s;
+	for (int i = 0; i < n; i++)
 	{
-		scanf("%d",&data[i]);
+		cin >> value[i];
 	}
-	back(0,0);
-	if(s == 0)
-	{
-		count--;
-	}
-	printf("%d\n",count);
+	sub(0,0,0,false);
+	cout << ans;
 }
