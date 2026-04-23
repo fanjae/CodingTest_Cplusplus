8df26a67815ac5f6c9985d53ccb1bb742eb5807a
FanJae <hjjang45@gmail.com>
2026-04-23 21:17:13 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,62 +1,9 @@
 #include <string>
 #include <vector>
-#include <queue>
-#include <iostream>
-using namespace std;
-queue<pair<string, int>> que;
-bool visited[55] = { 0 };
-int bfs(string begin, string target, vector<string> &words)
-{
-    int word_len = begin.length();
-    que.push({begin,0 });
-    while (!que.empty())
-    {
-        string front_string = que.front().first;
-        int answer = que.front().second;
-        
-        if (front_string == target)
-        {
-            return answer;
-        }
 
-        que.pop();
-        for (int i = 0; i < words.size(); i++)
-        {
-            if (visited[i] == true) continue;
-            else
-            {
-                int cnt = 0;
-                for (int j = 0; j < word_len; j++)
-                {
-                    if (front_string[j] != words[i][j])
-                    {
-                        cnt++;
-                    }
-                }
-                if (cnt == 1)
-                {
-                    visited[i] = true;
-                    que.push({ words[i],answer + 1 });
-                }
-            }
-        }
-    }
-}
+using namespace std;
+
 int solution(string begin, string target, vector<string> words) {
     int answer = 0;
-    bool impossible = true;
-
-    for (int i = 0; i < words.size(); i++)
-    {
-        if (target == words[i]) // 하나라도 같은 단어 있으면 만들 수 있음.
-        {
-            impossible = false;
-        }
-    }
-
-    if (impossible == false)
-    {
-        answer = bfs(begin, target, words);
-    }
     return answer;
 }
